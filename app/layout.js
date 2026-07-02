0b9257c2487174e1aae012d7a81c783726ef6ee9
Monadmoph <ihedioranmapromise@gmail.com>
2026-07-02 06:39:20 -0700

    Integrate Google Analytics into RootLayout
    Added Google Analytics scripts to the RootLayout component.

--- app/layout.js
+++ app/layout.js
@@ -15,3 +15,28 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
+import Script from "next/script";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-C7DX7WTH30"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-C7DX7WTH30');
+          `}
+        </Script>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
